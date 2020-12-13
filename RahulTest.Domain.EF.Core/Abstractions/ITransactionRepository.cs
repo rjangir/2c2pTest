5584ffcd094852715037102666e1a0f99d77da30
Rahul Jangir <rjangir.dev@gmail.com>
2020-12-13 15:09:10 +0700

    Saving transactions to database imported from csv file

---
+++ RahulTest.Domain.EF.Core/Abstractions/ITransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using RahulTest.Domain.EF.Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace RahulTest.Domain.EF.Core.Abstractions
+{
+    public interface ITransactionRepository
+    {
+        void AddTransactions(IList<Transaction> transactions);
+        IEnumerable<Transaction> GetTransactions(Func<Transaction, bool> predicate);
+    }
+}
